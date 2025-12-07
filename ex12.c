36270b51ce52dbde5b76049092a3fb0b0deaad1f
suraglr <elif.sura.glr3@gmail.com>
2025-12-07 22:59:40 +0300

    Elif Şura Güler - ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    int factorial = 1;
+
+    do {
+        printf("Enter a number less than 50: ");
+        scanf("%d", &n);
+
+        if (n < 0 || n >= 50)
+            printf("Invalid input. Try again.\n");
+
+    } while (n < 0 || n >= 50);
+
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+
+    printf("Factorial of %d is %d\n", n, factorial);
+
+    return 0;
+}
