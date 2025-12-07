8a4ceb2444a1a2fba507c95a419dfb9c024e7dda
suraglr <elif.sura.glr3@gmail.com>
2025-12-07 22:27:28 +0300

    Elif Şura Güler - ex8.c

--- ex08.c
+++ ex08.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[10];
+    int sum = 0;
+    float average;
+
+  
+    for (int i = 0; i < 10; i++) {
+        printf("Enter the number %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+        sum += numbers[i];
+    }
+
+    printf("Numbers: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", numbers[i]);
+    }
+
+    printf("\nSum is : %d", sum);
+
+    average = sum / 10;
+    printf("\nAverage is : %.2f\n", average);
+
+    return 0;
+}
