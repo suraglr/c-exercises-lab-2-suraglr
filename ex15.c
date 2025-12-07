1a34bc0e38730da2bb0f05a3b2efc7c0999df0ea
suraglr <elif.sura.glr3@gmail.com>
2025-12-07 23:16:56 +0300

    Elif Şura Güler - ex15.c

--- ex15.c
+++ ex15.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main() {
+    int a, b, c;
+
+    for (a = 1; a <= 5; a++) {
+
+        for (b = 1; b <= 5 - a; b++)
+            printf(" ");
+
+        for (c = 1; c <= a; c++)
+            printf("%d", c);
+
+        for (c = a - 1; c >= 1; c--)
+            printf("%d", c);
+
+        printf("\n");
+    }
+
+
+  for (a = 4; a >= 1; a--) {
+
+        for (b = 1; b <= 5 - a; b++)
+            printf(" ");
+
+        for (c = 1; c <= a; c++)
+            printf("%d", c);
+
+        for (c = a - 1; c >= 1; c--)
+            printf("%d", c);
+
+        printf("\n");
+    }
+
+    return 0;
+}
