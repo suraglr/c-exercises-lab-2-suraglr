fa83129a76aaf061337340d0d9cb39f6c8aca24c
suraglr <elif.sura.glr3@gmail.com>
2025-12-07 22:21:59 +0300

    Elif Şura Güler - ex7.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        int column1 = i * 5;
+        int column2 = 100 - ((i - 1) * 10 + 1);
+        int column3 = i;
+
+        printf("%d  %d  %d\n", column1, column2, column3);
+    }
+
+    return 0;
+}
